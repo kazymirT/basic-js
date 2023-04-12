610e2ad93923350ee429e094afda1130f093ac60
Taras <kazemir.t23.23@gmail.com>
2023-04-12 21:19:12 +0300

    feat: added  solution  tasks: mine-sweeper and what-season

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -24,7 +24,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]
  */
 function minesweeper(matrix) {
-  
+  matrix.forEach(e => {
+          for(let i=0;i<e.length;i++){
+              if(e[i]==false){
+                  e[i]=0
+              }else{
+                  e[i]=1;
+              }
+          }
+      }
+  );
+  if(matrix.flat().indexOf(1)) return matrix;
+  for(let i=0;i<matrix.length;i++){
+      if(matrix[i].indexOf(1)!=-1){
+          let index1=i;
+          let index2=matrix[i].indexOf(1)
+          if(index1==0){
+              matrix[index1+1][index2]=+1;
+              matrix[index1][index2+1]=+1;
+          }
+          if(index1==1){
+              matrix[index1+1][index2]+=1;
+              matrix[index1+1][index2+1]+=1;
+              matrix[index1+1][index2+2]+=1;
+              matrix[index1][index2+2]+=1;
+              matrix[index1][index2]+=1;
+              matrix[index1-1][index2+1]+=1;
+              matrix[index1-1][index2+2]+=1;
+          }
+      }
+  }
+  return matrix
 }
 
 module.exports = {
--- src/what-season.js
+++ src/what-season.js
@@ -11,30 +11,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-
-// if(!date) return "Unable to determine the time of year!";
-// if (!(date instanceof Date)){
-//   throw new Error('Invalid date!');
-//   }
-// let season;
-// let month=date.getMonth();
-// switch(month){
-//   case 0:
-//   case 1:
-//   case 11: season="winter"; break;
-//   case 2:
-//   case 3: 
-//   case 4: season="spring"; break;
-//   case 5: 
-//   case 6: 
-//   case 7: season='summer';break; 
-//   case 8: 
-//   case 9: 
-//   case 10: season="autumn";break; 
-// }
-// return season;
+function getSeason(date) {
+  if(!date) return "Unable to determine the time of year!";
+  if(date.toString!=new Date().toString){
+throw new Error('Invalid date!');
+};
+if (!(date instanceof Date)){
+throw new Error('Invalid date!');
+}
+let season;
+let month=date.getMonth();
+switch(month){
+case 0:
+case 1:
+case 11: season="winter"; break;
+case 2:
+case 3: 
+case 4: season="spring"; break;
+case 5: 
+case 6: 
+case 7: season='summer';break; 
+case 8: 
+case 9: 
+case 10: season="autumn";break; 
+}
+return season;
 }
 
 module.exports = {
